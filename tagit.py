d2d6f61fdf98e709efe3cc6d2501b88187317b5d
Johan Henriksson <jhn98032@gmail.com>
2015-03-13 22:21:57 +0100

    added tagit.py

---
+++ tagit.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+
+
+with open("src/version.h", "r") as f:
+    minor = ""
+    major = ""
+    patch = ""
+    lines = f.readlines()
+    for row in lines:
+        row = row.lstrip()
+
+        # Seperate the '#' character
+        if len(row) > 0 and row[0] == '#':
+            row = row[0] + ' ' + row[1:]
+
+        # Split the row into words
+        words = row.split()
+
+        # Decode line
+        if row.find("MAJOR") != -1:
+            major = words[3]
+        if row.find("MINOR") != -1:
+            minor = words[3]
+        if row.find("PATCH") != -1:
+            patch = words[3]
+
+    if minor != "" and major != "" and patch != "":
+        tagName = ("rel-%s.%s.%s" % (major, minor, patch))
+        os.system("git tag %s" % (tagName))
+
+
+
+
