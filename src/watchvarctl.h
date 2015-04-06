141d5f8940ff8848aedc3061c8169c2c43493af8
Johan Henriksson <jhn98032@gmail.com>
2015-04-07 00:01:44 +0200

    Added license text in all files

--- src/watchvarctl.h
+++ src/watchvarctl.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2014-2015 Johan Henriksson.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+
 #ifndef WATCHVAR_CTL_H
 #define WATCHVAR_CTL_H
 
