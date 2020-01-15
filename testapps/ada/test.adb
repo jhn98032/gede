54ac44855ce9ef3f9f99247d215ec3f1832c307e
Johan Henriksson <jhn98032@gmail.com>
2020-01-15 20:44:28 +0100

    Added Ada test example

---
+++ testapps/ada/test.adb
@@ -0,0 +1,13 @@
+with Text_IO; use Text_IO;
+procedure test is
+begin
+   
+    
+    Put_Line("Hello world!!");
+    
+    for i in 1.. 10 loop
+        Put_Line("loop");
+    end loop;
+
+end test;
+
