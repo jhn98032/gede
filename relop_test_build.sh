27b76673caa5b47e6fa821f29fdc27d87ea7f977
Johan Henriksson <jhn98032@gmail.com>
2020-03-15 12:21:10 +0100

    Updated test build script

--- relop_test_build.sh
+++ relop_test_build.sh
@@ -7,3 +7,16 @@ set -e
 ./build.py clean
 ./build.py --verbose --use-qt5
 
+#
+for td in testapps/*;do
+    if [ -d "$td" ]; then
+      echo "$td"
+      pushd $td > /dev/null
+      make
+      popd > /dev/null
+    fi
+done
+
+echo "All builds ok"
+
+
